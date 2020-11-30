0af86ecbc0b8505e5c88cd5dde1f614261bbfee2
Hans Geel <mcmacker4@gmail.com>
2020-11-30 11:56:36 +0100

    Clone Tmux Plugin Manager.

--- install.fish
+++ install.fish
@@ -67,9 +67,8 @@ end
 function tmux --description "Install Tmux package and link config files."
     if confirm "Use Tmux?"
         install tmux
-
+        git clone https://github.com/.tmux/plugins/tpm
         linkfile ".tmux.conf"
-
     end
 end
 
